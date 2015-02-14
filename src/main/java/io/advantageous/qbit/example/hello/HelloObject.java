5115cccc3d2cdaf264161698972b2d9986c28022
fadihub <fadimaalouli@gmail.com>
2015-02-13 16:20:09 -0800

    debugging

---
+++ src/main/java/io/advantageous/qbit/example/hello/HelloObject.java
@@ -0,0 +1,17 @@
+package io.advantageous.qbit.example.hello;
+
+/**
+ * Created by rhightower on 2/10/15.
+ */
+
+
+public class HelloObject {
+    private final String hello;
+    private final long time = System.currentTimeMillis();
+
+    public HelloObject(String hello) {
+        this.hello = hello;
+    }
+}
+
+
